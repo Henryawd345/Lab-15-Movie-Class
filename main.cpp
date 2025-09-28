502e76b3355a5ea427c7ced9e024e19296a1d485
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-27 22:30:06 -0700

    class working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <array>
 #include <iomanip>
 
 using namespace std;
+
+class Movie{
+    private:
+        string screenWriter;
+        int yearReleased;
+        string titleCard;
+
+    public:
+        string getScreenwriter() {return screenWriter;}
+        int getYearreleased() {return yearReleased;}
+        string getTitlecard() {return titleCard;}
+        void setScreenwriter(string sw) {screenWriter = sw;}
+        void setYearreleased(int yr) {yearReleased = yr;}
+        void setTitlecard(string tc) {titleCard = tc;}
+
+
+};
